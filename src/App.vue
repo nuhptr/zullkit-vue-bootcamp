77a90384451e5381cea47158e35d7abaae1ac549
nuhptr <nugrahaadi733@gmail.com>
2022-11-10 13:47:58 +0700

    slicing home done

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ import { RouterLink, RouterView } from "vue-router";
           <li>
             <RouterLink
               to="/"
-              class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white"
+              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               aria-current="page"
               >Home</RouterLink
             >
@@ -62,7 +62,7 @@ import { RouterLink, RouterView } from "vue-router";
           </li>
           <li>
             <RouterLink
-              to="#"
+              to="/studycase"
               class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Study Case</RouterLink
             >
